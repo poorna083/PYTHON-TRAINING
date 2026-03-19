c4e05c401c6480e6efa30846e4f1872068c1c4ec
poorna083 <dpoorna083@gmail.com>
2026-03-19 19:41:10 +0530

    indexing

---
+++ DAY1/strings/indexing.py
@@ -0,0 +1,9 @@
+# INDEXING:
+#   *it is the process of extracting individual character from a collection is known as 
+# INDEXING
+
+# *it raises index error if the specified index is not present
+
+# syntax:
+#        variable_name/collection [index]
+
