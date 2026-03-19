eccf703a34099963e3050f07e61828cedc94e5a3
poorna083 <dpoorna083@gmail.com>
2026-03-19 19:55:28 +0530

    slicing

---
+++ DAY1/strings/slicing.py
@@ -0,0 +1,10 @@
+# SLICING:
+#       it is the process of extracting more thsn one character from a collectionis known as SLICING
+
+# syntex:
+                                 
+#                     staring_index [0]     step_value +1  
+#                                  |          |
+#        variable_name/collection[[si:][ei]:[sv]]
+#                                        |
+#                                   end_index(len())
