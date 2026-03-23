d20064aeda2474458f0b2af0b6b389df846892b1
poorna083 <dpoorna083@gmail.com>
2026-03-23 22:57:44 +0530

    title

---
+++ DAY3/strings/title.py
@@ -0,0 +1,8 @@
+# title():
+#    it is used to convert the initial letter into uppercase of every word and remaining as lower
+
+s = "hello how are you"
+print(s.title())
+
+s1="!hello @how #are $you********"
+print(s1.title())
