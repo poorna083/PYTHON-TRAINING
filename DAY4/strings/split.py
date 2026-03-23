f71f28aa918951c6adfde1e0c7bcb6578c2e017f
poorna083 <dpoorna083@gmail.com>
2026-03-23 23:17:06 +0530

    split

---
+++ DAY4/strings/split.py
@@ -0,0 +1,13 @@
+# split():
+#      it is used to split the given string based on the saperator and maximum splits
+#      the return type of split is list
+
+
+
+# syntax:
+# var_name.split([saperator],[max_splits])
+
+s="hello every one"
+print(s.split())
+print(s.split(maxsplit=1))
+print(s.split('e'))
