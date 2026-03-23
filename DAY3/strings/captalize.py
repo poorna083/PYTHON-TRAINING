77dd3ee3cd0273036b7f536e89f1dcbd14f2e58d
poorna083 <dpoorna083@gmail.com>
2026-03-23 22:54:48 +0530

    captalize

---
+++ DAY3/strings/captalize.py
@@ -0,0 +1,8 @@
+# captalize:
+#     it is used to convert the initial character into upper case and remaining all into smallercase
+
+s = "hello"
+print(s.capitalize())
+
+s1="@hello hi"
+print(s1.capitalize())
