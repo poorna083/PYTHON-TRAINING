9b2d5d5fd5e7d3f57a1d6e613dfc2d56d8523585
poorna083 <dpoorna083@gmail.com>
2026-03-23 23:32:07 +0530

    join

---
+++ DAY4/strings/join.py
@@ -0,0 +1,16 @@
+# join():
+#      it is used to add the specified in between the element present in the given collection
+#      the return type of join is string
+
+# syntax:
+# "string".join(collection)
+
+# s="malayalam"
+# print("-".join(s))
+
+
+# Q) convert the 'hello every one" into "one every hello"
+s1 = "hello every one"
+lst = s1.split()
+res = lst[::-1]
+print(' '.join(res))
