6133e7f728553b9a21bd656c883b8338e9763df4
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:19:59 +0530

    isalnum

---
+++ DAY5/strings/isalnum.py
@@ -0,0 +1,11 @@
+# isalnum():
+# it returns true if the given string (or) character contains only  alphabets or numeric and alphanumericvalues
+# it wont accept any argument
+#the return value is boolean
+
+s="hello12"
+print(s.isalnum())
+s1="HELLO12"
+print(s1.isalnum())
+s2="234"
+print(s2.isalnum())
