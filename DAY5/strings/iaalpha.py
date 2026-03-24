87ab8e8dd0181632ebdf77427f1b858984307785
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:13:50 +0530

    isalpha

---
+++ DAY5/strings/iaalpha.py
@@ -0,0 +1,11 @@
+# isalpha:
+    # it returns true if the given string (or) character contains only  alphabets
+# it wont accept any argument
+#the return value is boolean
+
+s="hello"
+print(s.isalpha())
+s1="$"
+print(s1.isalpha())
+s2="HELLO"
+print(s2.isalpha())
