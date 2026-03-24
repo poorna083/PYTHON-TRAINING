a3025907a9958fcdcf93fd0eac68a330f6df8d62
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:11:11 +0530

    lower

---
+++ DAY5/strings/islower.py
@@ -0,0 +1,11 @@
+# islower:
+#      it returns true if the given string (or) character contains only lowercase alphabets
+#      it wont accept any argument
+#      the return value is boolean
+
+s="hello"
+print(s.islower())
+s1="hello@!$"
+print(s1.islower())
+s2="HEllO"
+print(s2.islower())
---
+++ DAY5/strings/isupper.py
@@ -0,0 +1,16 @@
+# isupper:
+#      it returns true if the given string (or) character contains only uppercase alphabets
+#      it wont accept any argument
+#      the return value is boolean
+
+s="HELLO"
+print(s.isupper())
+s1="HELLO@!$"
+print(s1.isupper())
+s2="HEllO"
+print(s2.isupper())
+
+
+
+
+
