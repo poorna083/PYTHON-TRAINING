1a69e167b180a770524d172cd1e5d0428d5a34f5
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:06:50 +0530

    find

---
+++ DAY5/strings/find.py
@@ -0,0 +1,13 @@
+# find():
+#      it is used to return the position of first occured specified element
+#      from the leftside
+
+#     it returns -1 if the element is not present
+
+
+s="hello"
+print(s.find('l'))
+
+print(s.find('z'))
+
+  
---
+++ DAY5/strings/rfind.py
@@ -0,0 +1,13 @@
+# rfind():
+#      it is used to return the position of first occured specified element
+#      from the rightside
+
+#     it returns -1 if the element is not present
+
+
+s="hello"
+print(s.rfind('l'))
+
+print(s.rfind('z'))
+
+  
