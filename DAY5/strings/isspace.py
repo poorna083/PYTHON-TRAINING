2c04ea55ee347d8e0515a0f0c0cde47c419cfc93
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:22:44 +0530

    space

---
+++ DAY5/strings/isspace.py
@@ -0,0 +1,11 @@
+# isspace():
+#     it returns true the given contains only have space
+#     it wont accept any argument
+#     the return type of isspace is boolean
+
+s="hello"
+print(s.isspace())
+s1="234"
+print(s1.isspace())
+s2=" "
+print(s2.isspace())
