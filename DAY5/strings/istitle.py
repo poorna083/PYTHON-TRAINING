0d91db3c91a7e0a16197ae5b178e08f6dfe4e743
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:26:33 +0530

    istitle

---
+++ DAY5/strings/istitle.py
@@ -0,0 +1,10 @@
+# istitle():
+# it returns true if the given string (or) character contains only  title .: the first char should capital and remaing should be small
+# it wont accept any argument
+#the return value is boolean
+
+s="Hello How Are You"
+print(s.istitle())
+
+s1="hello hOW are You"
+print(s1.istitle())
