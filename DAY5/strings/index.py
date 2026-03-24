c888311b56dc95eff21ae63d067ffc06875d2673
poorna083 <dpoorna083@gmail.com>
2026-03-24 20:03:29 +0530

    index

---
+++ DAY5/strings/index.py
@@ -0,0 +1,7 @@
+# index():
+#     it is used to return the first occured element from the left side
+
+s="programming"
+print(s.index('m'))
+
+print(s.index('m',4,9))
---
+++ DAY5/strings/rindex.py
@@ -0,0 +1,7 @@
+# rindex():
+#     it is used to return the first occured element from the right side
+
+s="programming"
+print(s.rindex('m'))
+
+print(s.rindex('m',4,9))
