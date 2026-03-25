237b2e92febc9c5dd7182fd079f4c752189de2b1
poorna083 <dpoorna083@gmail.com>
2026-03-25 21:36:58 +0530

    list

---
+++ DAY6/list/list1.py
@@ -0,0 +1,35 @@
+list=[1,2,3,4,5]
+print(list)
+
+#it is a collection of homogenius and heterogenius types of datas
+
+list1 = [1,2,"hello",True,3+4j,[1,3,5]]
+print(list1)
+
+#it is an ordered datatype
+#it allows indexing and slicing
+
+list2 = [1,2,3,4,4,3,2,1]
+print(list2)
+
+#it allows indexing and slicing
+
+print(list2[2])
+
+#the syntax of updating the element in al list is:
+
+# Syntax:
+    #  var_name[index]= value
+
+list2[3] = 76
+list2[5] = 86
+print(list2)
+
+lst=["apple","orange","mango","grapes"]
+ 
+
+# orange # man #gae
+
+print(lst[1])
+print(lst[2][:3:1])
+print(lst[3][::2])
