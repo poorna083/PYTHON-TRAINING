587785038a1436550a48d4c56128119b1b9e92e1
poorna083 <dpoorna083@gmail.com>
2026-03-26 23:03:14 +0530

    remove

---
+++ DAY7/remove.py
@@ -0,0 +1,13 @@
+# remove():
+#  it is used to remove the first occured specified element from the leftside
+# it also accept only one argument
+
+list=[1,2,3,4,5,6,7,1,4,3,2,7]
+list.remove(1)
+print(list)
+list.remove(4)
+print(list)
+list.remove(6)
+print(list)
+
+
