e61f5f723c2bf70e83da2ea0735bf9782b1efcc4
poorna083 <dpoorna083@gmail.com>
2026-03-26 22:52:36 +0530

    extend

---
+++ DAY7/extend.py
@@ -0,0 +1,12 @@
+# extend():
+# it is used to add the element atlast
+# it accept only collection datatypes
+# it will add the element by unpacking
+# it accept only one argument
+
+list=[1,2,3]
+print(list)
+list.extend([4,5,6])
+print(list)
+list.extend([[7,8,9]])
+print(list)
