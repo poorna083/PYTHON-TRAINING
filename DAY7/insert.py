c722d25175bba8435b285f3beb9f1005ea7d5ef1
poorna083 <dpoorna083@gmail.com>
2026-03-26 22:59:05 +0530

    insert

---
+++ DAY7/insert.py
@@ -0,0 +1,8 @@
+# insert():
+# *it is used to add the element at a particular element at an particular index
+list=[1,2,3,4]
+print(list)
+list.insert(3,44)
+print(list)
+list.insert(34,"hello")
+print(list)
