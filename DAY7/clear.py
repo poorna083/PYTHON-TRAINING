172c5c9ffc9749fd7ce51c96bdba7449b7c9d2b0
poorna083 <dpoorna083@gmail.com>
2026-03-26 23:09:38 +0530

    clear

---
+++ DAY7/clear.py
@@ -0,0 +1,17 @@
+# clear():
+#     it is used to remove all the elements
+#   it wont accept any arguments
+
+list=[1,2,3,4]
+list.clear()
+print(list)
+
+
+# del():
+   #it is used to remove the entire variable
+
+list1=[1,2,3,4,5,6]
+del list1
+
+
+print(list1)
