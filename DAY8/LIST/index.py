386dac405a09098e0c045ad10221fd343c95f341
poorna083 <dpoorna083@gmail.com>
2026-03-27 19:41:13 +0530

    indexing

---
+++ DAY8/LIST/index.py
@@ -0,0 +1,17 @@
+# indexing():
+#    it is used to return the index of an first occured specified element from the leftside
+
+# it accept three arguments:
+# *element
+# *startindex
+# *endindex
+
+# syntax:
+#    var_name.index(element,[si],[ei])
+
+list=[11,22,33,11,22,33,11,22,33]
+print(list.index(22))
+
+print(list.index(22,2,6))
+
+print(list.index(99))
