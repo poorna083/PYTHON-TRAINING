7417e81e831dbe43efedc4157dc9c3aff19500b2
poorna083 <dpoorna083@gmail.com>
2026-03-27 19:25:33 +0530

    reverse

---
+++ DAY8/LIST/reverse.py
@@ -0,0 +1,12 @@
+# reverse():
+#   it is used to reverse the given list and update it into the existing variable
+# syntax:
+#   var_name.reverse()
+
+list=[1,2,3,4]
+print(list[::-1])
+print (list)
+
+list =[1,2,3,4,5,6]
+list.reverse()
+print(list)
