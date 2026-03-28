bc567f48bbe446ce60c9a4315ad2cd6e3e32c029
poorna083 <dpoorna083@gmail.com>
2026-03-29 00:19:14 +0530

    indexing

---
+++ DAY9/tuples/count.py
@@ -0,0 +1,8 @@
+tuple=(1,2,3,1,2,3,1,2,3)
+print(tuple.count(2))
+
+print(tuple.count(67))
+
+# note: 
+#     when there are the element not found in the tuple then it is used to return 0
+
---
+++ DAY9/tuples/index.py
@@ -0,0 +1,6 @@
+tuple=(11,22,33,11,22,33)
+print(tuple.index(11))
+
+print(tuple.index(11,2,5))
+
+print(tuple.index(99))
