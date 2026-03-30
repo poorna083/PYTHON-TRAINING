6f7ea4b4a6339969c7fd325954a95b21c5bb6add
poorna083 <dpoorna083@gmail.com>
2026-03-30 19:34:49 +0530

    set

---
+++ DAY10/set/setmethods/add.py
@@ -0,0 +1,10 @@
+s={11,22,33,44}
+
+s.add('a')
+print(s)
+
+s.add(3)
+print(s)
+
+s.add([1,2,3])
+# print(s)                               ---------------------> error wiil occur
---
+++ DAY10/set/setmethods/pop.py
@@ -0,0 +1,12 @@
+# pop():
+
+s={"a","b",3,"hello"}
+
+print(s)
+
+print(s.pop())
+print(s.pop())
+print(s.pop())
+print(s.pop())
+
+print(s.pop())
