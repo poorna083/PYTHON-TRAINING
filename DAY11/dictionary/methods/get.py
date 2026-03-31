7a74881ec0e515c457fa2c457427a9b977cf66d8
poorna083 <dpoorna083@gmail.com>
2026-03-31 23:35:47 +0530

    get

---
+++ DAY11/dictionary/methods/get.py
@@ -0,0 +1,10 @@
+# get():
+# it is used to get the values based on the key specifiers
+# it returns none if the specifird key value is not present
+# it accept two arguments.they are:
+#          *key
+#          *value
+
+
+# syntax:
+#    variable_name.get(key,[value=None])
