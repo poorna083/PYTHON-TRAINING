f52ce793cf96b7de9087fe5d3587dc17832118c9
poorna083 <dpoorna083@gmail.com>
2026-03-31 23:38:07 +0530

    get

--- DAY11/dictionary/methods/get.py
+++ DAY11/dictionary/methods/get.py
@@ -8,3 +8,13 @@
 
 # syntax:
 #    variable_name.get(key,[value=None])
+
+
+d={'a':1,'b':2,'c':3}
+print(d.get('a'))
+
+print(d.get('g'))
+
+print(d.get('g',"stupid key is not present re check again"))
+
+print(d.get('b'))
