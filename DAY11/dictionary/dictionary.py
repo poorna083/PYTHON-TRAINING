af66c0ac1f2a3c5a657377d89013eed9b39eabb0
poorna083 <dpoorna083@gmail.com>
2026-03-31 23:30:33 +0530

    dictionary

---
+++ DAY11/dictionary/dictionary.py
@@ -0,0 +1,14 @@
+d={'a':1,'b':2,'c':3}
+print(d)
+
+d1={1:"hi",2.2:"bye",False:[1,2,3],3+5j:{1,2,3},'app':'insta',(1,2):'tuple'}
+print(d1)
+
+print("hi")             #error
+
+print(1)
+
+
+#update:
+
+d[1]="hi"
