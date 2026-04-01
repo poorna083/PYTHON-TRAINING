7b23750b0c5b0766bd0af36346b92478c906ac77
poorna083 <dpoorna083@gmail.com>
2026-04-01 18:42:38 +0530

    update

---
+++ DAY12/dictionary/update.py
@@ -0,0 +1,15 @@
+# update():
+#    it is used to add or update more than one keyword and value at a time into a dictionary
+#    update method will accept dictionary as a input
+
+d={'a':1,'b':2}
+d['c']=3
+print(d)
+
+d['a']=11
+print (d)
+
+d.update ({'d':3,'c':4,'e':5})
+print(d)
+
+
