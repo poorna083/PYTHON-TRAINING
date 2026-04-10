a21931d2031781e23bd9b33b2a2dc83ad4916a0e
poorna083 <dpoorna083@gmail.com>
2026-04-10 22:36:20 +0530

    ifelse

---
+++ DAY17/ifelse.py
@@ -0,0 +1,13 @@
+# if else:
+#     if condition:
+#         [TSB]
+#     else:
+#         [FSB]
+
+# WAP TO CHECK GIVEN NUMBER IS EVEN OR ODD
+no = int(input("enter the number:"))
+if no%2 ==0 :
+    print(f'{no} id even')
+else:
+    print(f'{no}is odd')
+print("executed")
