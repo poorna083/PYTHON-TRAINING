241513f8d70608a4a6eac0b2f22b589ecf69dfba
poorna083 <dpoorna083@gmail.com>
2026-04-12 19:31:44 +0530

    if

---
+++ DAY17/marks.py
@@ -0,0 +1,21 @@
+# wap to display the grade of student
+
+# 90 above-> A
+# 81-90 -> B
+# 71-80 -> C
+# 61-70 -> D
+# below 60 -> fail
+
+marks = int(input("enter the marks of the student"))
+if marks> 90:
+    print("A")
+elif marks >80:
+    print("B")
+elif marks >70:
+    print("B")
+elif marks >60:
+    print("B")
+elif marks >60:
+    print("fail")
+else:
+    print("valid marks enterd marks is invalid")
