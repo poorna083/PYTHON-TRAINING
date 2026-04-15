31280cd821deeeff93fe3260167be057e0c987ca
poorna083 <dpoorna083@gmail.com>
2026-04-15 23:01:31 +0530

    whileloop

---
+++ DAY19/whileloop.py
@@ -0,0 +1,32 @@
+# WAP TO PRINT HELLO PUTHON FOR FIVE TIMES
+# i=1
+# while i<=5:
+#     print("hello python")
+#     i+=1
+# WAP TO PRINT 1 TO 5 NUMBERS
+# i=1
+# while i<=5:
+#     print(i,end=" ")
+#     i+=1
+# WAP TO PRINT 5 TO 1 NUMBERS
+# i=5
+# while i>=1:
+#     print(i,end=" ")
+#     i-=1
+# WAP TO PRINT ALL THE EVEN NUMBERS FROM 1 TO 10
+# num=2
+# while num<=10:
+#     print(num,end=" ")
+#     num+=2
+# WAP TO PRINT ALL THE ODD NUMBERS FROM 1 TO 10
+# num=1
+# while num<=10:
+#     print(num,end=" ")
+#     num+=2
+# WAP TO PRINT A TABLE BY ASKING THE USER FROM THE INPUT OF WHICH TABLE
+
+tab_num=int(input("enter the table number  :"))
+i=1
+while i<=10:
+    print(f'{tab_num} * {i} = {tab_num*i}')
+    i+=1
