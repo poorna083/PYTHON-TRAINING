afdf5c5b91bd98fac6816b39f62a17494b4dc82e
poorna083 <dpoorna083@gmail.com>
2026-04-20 20:06:40 +0530

    whileloop

--- DAY21/whileloop.py
+++ DAY21/whileloop.py
@@ -20,52 +20,88 @@
 
 # WAP TO CHECK WHEATHER THE GIVEN NUMBER IS A AUTOMORPHIC NUMBER OR NOT
 # no=int(input("enter the number :"))
-# pow = no*no
-# if pow%10 == no:
+# temp = no
+# count = 0
+# square = no **2
+# while temp > 0:
+#     count+=1
+#     temp = temp//10
+# final_output = square %(10**count)
+# if no == final_output:
 #     print("automorphic number")
 # else:
 #     print("not a automorphic number")
 
 
+
+
+
 # WAP TO CHECK WHEATHER THE GIVEN NUMBER IS A XYLEM NUMBER AND PHONM NUMBER
-no=int(input("enter the number :"))
-count = 0
-temp1 = no
-temp2 = no
-temp3 = no
-temp4 = no
-while temp1 > 0:
-    temp1 = temp1 //10
-    count +=1
+# no = int(input("enter the number :"))
+# temp = no
+# temp1 = no
+# sum = 0
+# count = 0
+# while temp > 0:
+#     temp = temp//10
+#     count +=1
+# last_elem = temp1 %10
+# first_elem = temp1 // (10**(count-1))
+# sum_of_end = last_elem+first_elem
 
-div = 1
-i = 1
-while i < count:
-    div = div * 10
-    i += 1
+# middle = (no % (10 ** (count - 1))) // 10
+# while middle > 0:
+#     rem = middle % 10
+#     sum += rem
+#     middle = middle//10
+# if sum == sum_of_end:
+#     print("xylem")
+# else:
+#     print("phonem")
 
-first_elem = temp3 // div
-last_elem  = temp2 %10
 
-sum_first_last = first_elem+last_elem
 
-temp4 = no // 10   
-middle_elem = 0
-pos = 2
 
-while temp4 > 0:
-    rem = temp4 % 10
 
-    if pos != count:   
-        middle_elem += rem
 
-    temp4 = temp4 // 10
-    pos += 1
 
-if sum_first_last == middle_elem:
-    print("xylem number")
-else:
-    print("phonm number")
+
+# no = int(input("enter the number  :"))
+# temp = no
+# sum_of_end=0
+# sum_of_mid=0
+# while no >0:
+#     rem = no %10
+#     if no == temp or no < 10:
+#         sum_of_end+=rem
+#     else:
+#         sum_of_mid+=rem
+#     no = no//10
+# if sum_of_end==sum_of_mid:
+#     print("xylem")
+# else:
+#     print("phonem")
+
+
+
+
+# WAP to show that the given number is a happy number or not
+
+# no = int(input("Enter the number  :"))
+# temp = no
+
+# while temp !=1 and temp !=4:
+#     sum = 0
+#     while temp > 0:
+#         rem = temp % 10
+#         sum = rem**rem
+#         temp = temp // 10
+#     temp = sum
+# if temp ==1 :
+#     print("happy number ")
+# else:
+#     print("Not an happy number")
+
 
 
 
---
+++ DAY22/whileloop.py
@@ -0,0 +1,34 @@
+# fibnocci series:
+# a=0
+# b=1
+# no=int(input("enter the no.of numbers to print :"))
+# while no>0:
+#     print(a,end=" ")
+#     c=a+b
+#     a=b
+#     b=c
+#     no -=1
+
+
+# WAP to traverse a String
+# str=input("enter the string  :")
+# i=0
+# while i < len(str):
+#     print(str[i])
+#     i+=1
+
+
+# WAP to traverse a list
+# list=['srh','rcb','gt','lsg','mi']
+# i=0
+# while i < len(list):
+#     print(list[i])
+#     i+=1
+
+
+# WAP to print the element in even index
+str=input("enter the string :")
+i=0
+while i < len(str):
+    print(str[i])
+    i+=2
